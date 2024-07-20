a9b573a40bf7e52ce374e968da9b1822939ba5a9
Daniel <dannystvincent@gmail.com>
2024-07-20 13:00:13 -0400

    Delete all unneed files

--- chromeExtention/background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onInstalled.addListener(function () {
-  // Listen for clicks to the extension icon
-  chrome.action.onClicked.addListener(function () {
-    // When the icon is clicked, create a new tab pointing to the index page
-    chrome.tabs.create({
-      url: chrome.runtime.getURL("/index.html"),
-    });
-  });
-});
